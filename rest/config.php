df9909ff6745885df5b00d89a91a6967d167fa4f
dzenimed <dzenana.mededovic@gmail.com>
2022-04-09 14:56:45 +0200

    add config file

---
+++ rest/config.php
@@ -0,0 +1,11 @@
+<?php
+class Config {
+  const DATE_FORMAT = "Y-m-d H:i:s";
+
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "events";
+  const DB_PASSWORD = "events123";
+  const DB_SCHEME = "events_db";
+}
+
+?>
