e3302ce73efde46e6e88b6df07dcc48e21daeb13
dzenimed <dzenana.mededovic@gmail.com>
2022-06-10 15:38:23 +0200

    configuring swagger

--- rest/index.php
+++ rest/index.php
@@ -7,14 +7,14 @@ require_once dirname(__FILE__).'/../vendor/autoload.php';
 //require_once dirname(__FILE__).'/dao/BaseDao.class.php';
 Flight::set('flight.log_errors', TRUE);
 
-
-Flight::route('/hello', function () {
-    echo 'hello world!';
-});
+// 
+// Flight::route('/hello', function () {
+//     echo 'hello world!';
+// });
 
 
 /* REST API documentation endpoint */
-Flight::route('GET /docs', function(){
+Flight::route('GET /docs.json', function(){
   $openapi = \OpenApi\scan(['routes']);
   header('Content-Type: application/json');
   echo $openapi->toJson();
--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -1,5 +1,15 @@
 <?php
 /**
- * @OA\Info(title="Vertex Events API Specs", version="0.1")
- */
+ * @OA\Info(title="Vertex Events API Specs", version="0.1",
+ *    @OA\Contact(
+ *    email="dzenana.mededovic@stu.ibu.edu.ba",
+*     name="Dzenana Medjedovic"
+*     )),
+ * @OA\OpenApi(
+*     @OA\Server(
+*     url="http://localhost/Events-Web-App/rest",
+*     description="Development Environment"
+*     )
+* )
+*/
 ?>
