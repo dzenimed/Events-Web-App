c89f1facd3dafc54990e3c26aa7418d93a291e85
dzenimed <dzenana.mededovic@gmail.com>
2022-07-12 22:40:07 +0200

    hosted db details

--- rest/config.php
+++ rest/config.php
@@ -2,10 +2,15 @@
 class Config {
   const DATE_FORMAT = "Y-m-d H:i:s";
 
-  const DB_HOST = "localhost";
-  const DB_USERNAME = "events";
-  const DB_PASSWORD = "events123";
-  const DB_SCHEME = "events_db";
+  // const DB_HOST = "localhost";
+  // const DB_USERNAME = "events";
+  // const DB_PASSWORD = "events123";
+  // const DB_SCHEME = "events_db";
+
+  public const DB_HOST = "eu-cdbr-west-02.cleardb.net";
+  public const DB_USERNAME = "b80b15d1d7ad17";
+  public const DB_PASSWORD = "4b91e993";
+  public const DB_SCHEME = "heroku_df1469867643cb1";
 
   public static function JWT_SECRET()
   {
