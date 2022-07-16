a3133a170117d1cf9b22025b4fa327e005be954e
dzenimed <dzenana.mededovic@gmail.com>
2022-07-16 12:36:47 +0200

    test change

--- rest/routes/EventRoutes.php
+++ rest/routes/EventRoutes.php
@@ -11,7 +11,7 @@
 Flight::route('GET /events', function () {
     $offset = Flight::query('offset', 0);
     $limit = Flight::query('limit', 25);
-    $search = Flight::query('search');
+    $search = Flight::query('search', '');
     $order = Flight::query('order', "-id");
 
     Flight::json(Flight::eventService()->get_events_by_name($search, $offset, $limit, $order));
