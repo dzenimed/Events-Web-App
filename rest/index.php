d8a7d53ef427e279469a81878420f8768e9b79d2
dzenimed <dzenana.mededovic@gmail.com>
2022-07-18 00:04:13 +0200

    code refactoring

--- rest/index.php
+++ rest/index.php
@@ -58,33 +58,42 @@ Flight::map('jwt', function ($user) {
 });
 
 // middleware method for login
-Flight::route('/*', function(){
-  // return TRUE;
+//Flight::route('/*', function(){
   // perform JWT decode
-  $path = Flight::request()->url;
-  if ($path == '/login' || $path == '/register' || $path == '/docs.json'|| $path == '/events' || $path == '/event/@id' || $path == '/event/@city') return TRUE; // exclude login route from middleware 
+   // $path = Flight::request()->url;
+   // if ($path == '/login' || $path == '/register' || $path == '/docs.json' || $path == '/events' || $path == '/event/@id'){
+   //   return TRUE;
+   // }
+   // exclude login route from middleware str_contains($path, '')
+   // if ($path == '/login' || $path == '/register' || $path == '/docs.json'){
+   //   return TRUE;
+   // }
+   // if ( str_contains($path, '/login') || str_contains($path, '/register') || str_contains($path, '/docs.json') || str_contains($path, '/events') || str_contains($path, '/event/@id')){
+   //   return TRUE;
+   // }
   // if ($path == '/admin/add/event'){
-  //   $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+  //   $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));    '%', $id, '%'
   //   if ($user['r'] != "ADMIN"){
   //       Flight::set('user', $decoded);
   //       return TRUE;
   //   }
   // }
-  $headers = getallheaders();
-  if (@!$headers['Authorization']){
-    Flight::json(["message" => "Authorization is missing"], 403);
-    return FALSE;
-  }else{
-    try {
-      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
-      Flight::set('user', $decoded);
-      return TRUE;
-    } catch (\Exception $e) {
-      Flight::json(["message" => "Authorization token is not valid"], 403);
-      return FALSE;
-    }
-  }
-});
+
+//   $headers = getallheaders();
+//   if (@!$headers['Authorization']){
+//     Flight::json(["message" => "Authorization is missing"], 403);
+//     return FALSE;
+//   }else{
+//     try {
+//       $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+//       Flight::set('user', $decoded);
+//       return TRUE;
+//     } catch (\Exception $e) {
+//       Flight::json(["message" => "Authorization token is not valid"], 403);
+//       return FALSE;
+//     }
+//   }
+// });
 
 require_once __DIR__.'/routes/UserRoutes.php';
 require_once __DIR__.'/routes/CompanyRoutes.php';
